5b31e8af5d4d7489058494003e13103fc03988fe
FAMILIA <FAMILIA@DESKTOP-II3SOL7>
2021-09-02 18:34:56 -0500

    agregamos numero a la resta

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
 
 		float suma = sumar (1.0f, 2.0f,3.0f);
 		System.out.println(suma);
-		float resta = restar (4.0f, 5.0f);
+		float resta = restar (4.0f, 5.0f, 2.0f);
 	}
 
 	public static  float sumar (float num1, float num2, float num3) {
 		return num1 + num2+ num3;
 	}
-	public static  float restar (float num1, float num2) {
-		return num1 -num2;
+	public static  float restar (float num1, float num2, float num3) {
+		return num1 -num2-num3;
 }
 }
